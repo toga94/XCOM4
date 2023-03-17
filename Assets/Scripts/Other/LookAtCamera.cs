b011a064dd633f168e0352ab1e6f369b0b01da1e
Toghrul Huseynzada <bhgames@ya.ru>
2023-03-17 17:35:18 +0400

    update

--- Assets/Scripts/Other/LookAtCamera.cs
+++ Assets/Scripts/Other/LookAtCamera.cs
@@ -1,30 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LookAtCamera : MonoBehaviour
 {
-
     [SerializeField] private bool invert;
-
-
     private Transform cameraTransform;
-
+    private Transform cachedTransform;
 
     private void Awake()
     {
         cameraTransform = Camera.main.transform;
+        cachedTransform = transform;
     }
 
     private void LateUpdate()
     {
-        if (invert)
-        {
-            Vector3 dirToCamera = (cameraTransform.position - transform.position).normalized;
-            transform.LookAt(transform.position + dirToCamera * -1);
-        } else
-        {
-            transform.LookAt(cameraTransform);
-        }
+        transform.LookAt(transform.position + cameraTransform.rotation * Vector3.forward, cameraTransform.rotation * Vector3.up);
     }
 }
