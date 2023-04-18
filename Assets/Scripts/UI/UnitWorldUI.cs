4fd7143ff05e076303600adc16425eb0bfaaab8a
Toghrul Huseynzada <bhgames@ya.ru>
2023-04-18 17:26:32 +0400

    Update

--- Assets/Scripts/UI/UnitWorldUI.cs
+++ Assets/Scripts/UI/UnitWorldUI.cs
@@ -26,12 +26,7 @@ public class UnitWorldUI : MonoBehaviour
     public float HealthBarOffsetYPercent = 10f;
     private List<GameObject> hpLine;
     private Canvas canvas;
-
-    private void Awake()
-    {
-
-
-    }
+    private RectTransform rectTransform;
     public void SetRoot(Transform value, GameObject canvasObj)
     {
         canvas = canvasObj.GetComponent<Canvas>();
@@ -62,7 +57,7 @@ public class UnitWorldUI : MonoBehaviour
 
         InventoryGrid.Instance.OnAnyUnitMovedInventoryPosition += UpdateText;
         InventoryGrid.Instance.OnAnyUnitSwappedInventoryPosition += UpdateText;
-
+        rectTransform = GetComponent<RectTransform>();
         //HealthBarOffsetZ = 7f;
     }
 
@@ -106,7 +101,7 @@ public class UnitWorldUI : MonoBehaviour
         }
         levelText.text = unit.GetUnitLevel.ToString();
     }
-
+    public Vector3 offset;
     private void Update()
     {
         if (hpDamageSldier.fillAmount != hpSldier.fillAmount)
@@ -115,16 +110,30 @@ public class UnitWorldUI : MonoBehaviour
         }
         Vector3 headPosition = unit.transform.position;
 
-        // Calculate the position of the health bar in screen space
-        Vector3 healthBarPosition = mainCamera.WorldToScreenPoint(headPosition);
-        healthBarPosition.y += HealthBarOffsetZ;
-        healthBarPosition.z = 0;
-
-        // Set the position of the health bar
-        transform.position = healthBarPosition;
+        rectTransform.anchoredPosition = WorldToCanvasPosition(canvas.GetComponent<RectTransform>(), mainCamera, headPosition + Vector3.up * HealthBarOffsetZ);
     }
 
+    private Vector2 WorldToCanvasPosition(RectTransform canvas, Camera camera, Vector3 position)
+    {
+        //Vector position (percentage from 0 to 1) considering camera size.
+        //For example (0,0) is lower left, middle is (0.5,0.5)
+        Vector2 temp = camera.WorldToViewportPoint(position);
 
+        //Calculate position considering our percentage, using our canvas size
+        //So if canvas size is (1100,500), and percentage is (0.5,0.5), current value will be (550,250)
+        temp.x *= canvas.sizeDelta.x;
+        temp.y *= canvas.sizeDelta.y;
+
+        //The result is ready, but, this result is correct if canvas recttransform pivot is 0,0 - left lower corner.
+        //But in reality its middle (0.5,0.5) by default, so we remove the amount considering cavnas rectransform pivot.
+        //We could multiply with constant 0.5, but we will actually read the value, so if custom rect transform is passed(with custom pivot) , 
+        //returned value will still be correct.
+
+        temp.x -= canvas.sizeDelta.x * canvas.pivot.x;
+        temp.y -= canvas.sizeDelta.y * canvas.pivot.y;
+
+        return temp;
+    }
     private IEnumerator UpdateElement()
     {
         yield return new WaitForSeconds(0.015f);
