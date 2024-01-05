178b7efbd994e425b242711d7fa558981fe35a82
Toghrul Huseynzada <bhgames@ya.ru>
2024-01-05 13:40:11 +0400

    TraitFix+ added level asset

--- Assets/Scripts/UI/TraitsUI.cs
+++ Assets/Scripts/UI/TraitsUI.cs
@@ -66,25 +66,47 @@ public class TraitsUI : Singleton<TraitsUI>
         return traitdata.traitSprite;
     }
 
-    public Color GetTraitSpriteColor(TraitType trait, int level)
+    //public Color GetTraitSpriteColor(TraitType trait, int level)
+    //{
+    //    TraitData traitdata = TraitDataManager.Instance.FetchTraitData(trait);
+    //    int maxStack = GetTraitMaxStack(trait);
+
+    //    int colorIndex = Mathf.Min(traitdata.traitEffectsLevel.Length - 1, level - 1);
+
+    //    Color[] traitColors = new Color[]
+    //    {
+    //        new Color(0f, 0f, 0f), // level 1 color
+    //        new Color(0f, 0f, 1f), // level 2 color
+    //        new Color(0f, 1f, 0f), // level 3 color
+    //        new Color(1f, 0.8f, 0f), // level 4 color
+    //        new Color(1f, 0f, 0f) // level 5 color
+    //    };
+
+    //    return traitColors[colorIndex];
+    //}
+    private Color GetTraitSpriteColor(TraitType trait, int level)
     {
         TraitData traitdata = TraitDataManager.Instance.FetchTraitData(trait);
         int maxStack = GetTraitMaxStack(trait);
 
-        int colorIndex = Mathf.Min(traitdata.traitEffectsLevel.Length - 1, level - 1);
-
-        Color[] traitColors = new Color[]
-        {
-            new Color(0f, 0f, 0f), // level 1 color
-            new Color(0f, 0f, 1f), // level 2 color
-            new Color(0f, 1f, 0f), // level 3 color
-            new Color(1f, 0.8f, 0f), // level 4 color
-            new Color(1f, 0f, 0f) // level 5 color
+        Color[] traitColors = new Color[] {
+        new Color(0f, 0f, 0f), // level 1 color
+        new Color(0f, 0f, 1f), // level 2 color
+        new Color(0f, 1f, 0f), // level 3 color
+        new Color(1f, 0.8f, 0f), // level 4 color
+        new Color(1f, 0f, 0f) // level 5 color
         };
 
+        int colorIndex = 0;
+        var traitEffectLevel = traitdata.traitEffectsLevel;
+        var allTraitEffects = traitdata.traitEffectsLevel;
+        if (traitEffectLevel.Length > 0 && allTraitEffects[0] <= level) colorIndex = 4;
+        else if (traitEffectLevel.Length > 1 && allTraitEffects[1] <= level) colorIndex = 3;
+        else if (traitEffectLevel.Length > 2 && allTraitEffects[2] <= level) colorIndex = 2;
+        else if (traitEffectLevel.Length > 3 && allTraitEffects[3] <= level) colorIndex = 1;
+        else if (traitEffectLevel.Length > 4 && allTraitEffects[4] <= level) colorIndex = 0;
         return traitColors[colorIndex];
     }
-
     private int GetTraitMaxStack(TraitType trait)
     {
         TraitData traitdata = TraitDataManager.Instance.FetchTraitData(trait);
